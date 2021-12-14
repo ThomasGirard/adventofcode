86ccd9a2b5b90579e51e80cd392f9a44eafcd4cf
ThomasGirard <arrg.ch@gmail.com>
2021-12-14 21:20:13 +0000

    Improves Day 14

--- adventofcode2021/Day14PolymerInsertion.hs
+++ adventofcode2021/Day14PolymerInsertion.hs
@@ -5,6 +5,7 @@ import Text.Parsec.Char
 import Text.Parsec.Combinator
 import Text.Parsec.String
 import Control.Applicative
+import Data.Function ((&))
 
 import qualified Data.Map.Strict as M
 
@@ -33,47 +34,50 @@ p_rule = do
 
 type Rule = (String, Char)
 
--- Part 1
-p1 = score . (!!10) . grow
-
-grow (s, rules) = iterate (step []) s where
-	step ls [] = reverse ls
-	step [] (r:rs) = step [r] rs
+-- Part 1 (exponential)
+p1 (s, rules) = score . (!!10) . iterate (step []) $ s where
+	step ls     []     = reverse ls
+	step []     (r:rs) = step [r] rs
 	step (l:ls) (r:rs) | Just c <- lookup [l,r] rules = step (r:c:l:ls) rs
 		
-score s = (last groups) - (head groups) where
+score s = last groups - head groups where
 	groups = sort . map length . group . sort $ s
-		
--- This will grow like crazy...
--- There are only 10 unique characters and so 100 unique possible pairs to consider.
--- Each step, each pair gives birth to two other pairs. Say if we have a rule XZ -> Y
--- Then 1 XZ = 1 XY + 1 YZ (and the XZ disappears)
--- We can keep track of the 100 pairs as a count and not have to compute the exponential string.
-type PairCounts = M.Map String Integer
-
-p2 (s, rules) = score2 . (!!40) . iterate step2 . initCounts $ s where
-	-- The last character must be added to the count, since we only count the first char of each pair
-	score2 pm = last groups - head groups where
-		groups = sort . map (sum . map snd) . H.groupOn fst . sort . ((last s,1):) . map (\([x,y],n) -> (x,n)) . M.toList $ pm
-		
-	rules2 = processRules rules
 	
-	-- Fold over all current pairs, generating a new map with the children pair counts
-	step2 pm = M.foldrWithKey grow1 M.empty pm
-	-- For each existing pair with count N, add 2*N new pairs (N left, N right)
-	grow1 k v pm = let (a, b) = lookupJust k rules2 in
-		M.alter (inc v) a . M.alter (inc v) b $ pm
-
-inc v Nothing = Just v
-inc v (Just x) = Just (v+x)
+		
+-- Part2
+-- Each step, each pair creates to two new pairs. 
+-- E.g. with a rule XZ -> Y then XZ -> XY + YZ (and the XZ disappears).
+-- We can count the instances for each of the distinct pairs and not have to compute the string.
+type PairCounts = M.Map String Integer
+type Rule2 = (String, (String, String))
 
 initCounts :: String -> PairCounts
-initCounts s = M.fromList . map (\p -> (p, 1)) $ pairs where
-	pairs = takeWhile ((==2) . length) . map (take 2) . tails $ s -- Not cute
-		
--- Transform each original rule of the form XZ -> Y 
--- Into a more convenient pair XZ -> XY + YZ
-processRules = map processRule
-processRule (s@[l,r], c) = (s, ([l,c], [c,r])) 
-type Rule2 = (String, (String, String))
+initCounts s = M.fromList . map (\p -> (p, 1)) . pairs $ s where
+	pairs (x:y:z) = [x,y]:(pairs (y:z)) 	
+	pairs _ = []
+
+p2 (seed, rules) = score2 . (!!40) . iterate step2 . initCounts $ seed where
 	
+	-- Transform each original rule of the form XZ -> Y 
+	-- Into a more convenient pair XZ -> XY + YZ
+	rules' :: [Rule2]
+	rules' = map processRule rules
+	processRule (s@[l,r], c) = (s, ([l,c], [c,r])) 
+	
+	-- Fold over all current pairs, generating a new map with the children pair counts
+	-- For each existing pair with count v, add 2*v new pairs (v left, v right)
+	step2 = M.foldrWithKey grow1 M.empty
+	grow1 xz v pm = let (xy, yz) = lookupJust xz rules' in
+		  M.insertWith (+) yz v
+		. M.insertWith (+) xy v
+		$ pm
+		
+	score2 pm = last groups - head groups where
+		groups = pm  
+			& M.toList
+			& map (\([x,y],n) -> (x,n)) 
+			& ((last seed, 1):) -- Add the last char, because only first chars are counted otherwise
+			& sort 
+			& H.groupOn fst 
+			& map (sum . map snd)
+			& sort
