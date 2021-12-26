efd38bab132a8e7859258307c5626d19dad282f7
ThomasGirard <arrg.ch@gmail.com>
2021-12-26 19:51:13 +0000

    Day 5 part 2 (jumps/booleans)

--- adventofcode2019/IntCodeRW/IntCode.hs
+++ adventofcode2019/IntCodeRW/IntCode.hs
@@ -2,7 +2,7 @@ module IntCode (
 	Val(..),
 	Adr(..),
 	Ptr(..),
-	ReadOp(..),
+	ReadAdr(..),
 	execFully,
 	initMem,
 	readOp,
@@ -28,25 +28,35 @@ newtype Adr = Adr Int
 newtype Ptr = Ptr Int
 	deriving (Num, Ord, Eq, Show)
 	
+valAsPtr (Val v) = Ptr v
 ptrAsAdr (Ptr v) = Adr v
 valAsAdr (Val a) = Adr a
 valToInt (Val a) = a
 
-data ReadOp = 
+data ReadAdr = 
 	  Position Adr
 	| Immediate Val
 	deriving (Ord, Eq, Show)
 
 data Instr = 
 	  Halt 
-	| BinOp Op ReadOp ReadOp Adr
+	| BinOp Op ReadAdr ReadAdr Adr
 	| Input Adr
-	| Output ReadOp 
+	| Output ReadAdr 
+	| JmpIf JmpOp ReadAdr ReadAdr
 	deriving (Ord, Eq, Show)
+	
+data JmpOp = JmpIfTrue | JmpIfFalse 
+	deriving (Ord, Eq, Show)
+
+jmpFun JmpIfTrue = (/=0)
+jmpFun JmpIfFalse = (==0)
 
 data Op = 
 	  Add
 	| Mul
+	| CmpLT
+	| CmpEQ
 	deriving (Ord, Eq, Show)
 	
 type Mem = M.Map Adr Val
@@ -62,8 +72,13 @@ decodeInstruction mem p = case code of
 	99 -> (Halt, p)
 	1 -> decodeBinOp Add 
 	2 -> decodeBinOp Mul
+	7 -> decodeBinOp CmpLT
+	8 -> decodeBinOp CmpEQ
 	3 -> (Input (param 1), p+2)
 	4 -> (Output (decRead mode1 1), p+2)
+	5 -> (JmpIf JmpIfTrue (decRead mode1 1) (decRead mode2 2), p+3)
+	6 -> (JmpIf JmpIfFalse (decRead mode1 1) (decRead mode2 2), p+3)
+	n -> error $ "Unknown opcode " ++ (show n)
 	where
 	(code, mode1, mode2, mode3) = decodeCode $ mem M.! q
 	param n = valAsAdr $ mem M.! (q+n)
@@ -71,7 +86,7 @@ decodeInstruction mem p = case code of
 	decodeBinOp op = (BinOp op (decRead mode1 1) (decRead mode2 2) (param 3), p+4)
 	decRead DImm n = Immediate $ mem M.! (q+n)
 	decRead DPos n = Position $ valAsAdr $ mem M.! (q+n)
-	
+
 data DecReadMode = DPos | DImm 	
 	deriving (Ord, Eq, Show)
 
@@ -91,13 +106,19 @@ leftPad c n s | length s < n = leftPad c n (c:s)
 	
 exec1 :: VMState -> (ExecStatus, VMState)
 exec1 vm = case decodeInstruction (vMem vm) (vPtr vm) of
-	(instr, p') -> let (es, vm') = execInstr instr vm in (es, vm' {vPtr = p'})
+	(instr, p') -> let (es, vm') = execInstr instr (vm {vPtr = p'}) in (es, vm')
 	
 execInstr :: Instr -> VMState -> (ExecStatus, VMState)
 execInstr Halt vm = (Halted, vm)
 execInstr (Output a) vm = (HasOutput o, vm') where
 	o = valToInt $ readOp a (vMem vm)
 	vm' = vm {vOut = (vOut vm ++ [o])}
+execInstr (JmpIf jm a b) vm = (Cont, vm {vPtr = ptr'}) where
+	ptr' = if (jmpFun jm) valA then valB else vPtr vm
+	mem = vMem vm
+	valA = readOp a mem
+	valB = valAsPtr $ readOp b mem
+
 execInstr instr vm = (Cont, vm') where
 	mem = vMem vm
 	vm' = case instr of
@@ -106,6 +127,8 @@ execInstr instr vm = (Cont, vm') where
 			f = case op of
 				Add -> (+)
 				Mul -> (*)
+				CmpLT -> (boolToInt .) . (<)
+				CmpEQ -> (boolToInt .) . (==)
 		Input w -> case vTape vm of
 			[] -> error "Tried to read from empty tape"
 			(t:tape') -> vm {
@@ -113,7 +136,10 @@ execInstr instr vm = (Cont, vm') where
 				vTape = tape'
 				}
 
-readOp :: ReadOp -> Mem -> Val
+boolToInt True = 1
+boolToInt False = 0
+				
+readOp :: ReadAdr -> Mem -> Val
 readOp (Position adr) mem = mem M.! adr
 readOp (Immediate val) _ = val
 
